b2db64e51d48b9cb59e8deadd16b65416061102b
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2023-11-25 22:43:04 -0300

    feat: info endpoint and tests

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,7 +1,30 @@
+import { ConfigService } from '@nestjs/config';
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
 describe('AppController', () => {
-  describe('root', () => {
-    it('example test', () => {
-      expect(true).toBe(true);
+  let appController: AppController;
+  let appService: AppService;
+  let configService: ConfigService;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [AppService, ConfigService],
+    }).compile();
+
+    appController = app.get<AppController>(AppController);
+    appService = app.get<AppService>(AppService);
+    configService = app.get<ConfigService>(ConfigService);
+  });
+
+  describe('index', () => {
+    it('should return the app info', () => {
+      expect(appService.index()).toEqual({
+        name: configService.get('APP_NAME'),
+        author: configService.get('APP_AUTHOR'),
+      });
     });
   });
 });
--- src/app.service.ts
+++ src/app.service.ts
@@ -4,4 +4,11 @@ import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class AppService {
   constructor(private readonly configService: ConfigService) {}
+
+  index(): object {
+    return {
+      name: this.configService.get('APP_NAME'),
+      author: this.configService.get('APP_AUTHOR'),
+    };
+  }
 }
